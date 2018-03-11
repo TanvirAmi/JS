8d0fe37d78b4af5aceff1065ab2275e32a2f47ef
Nahidul Islam Tanvir <tanvir@Nahiduls-MacBook-Air.local>
2018-03-11 23:56:26 +0600

    Added a function name sum

--- closure/src/function.js
+++ closure/src/function.js
@@ -9,3 +9,16 @@ function myFunc(){
 }
 var x = myFunc();
 x();
+
+//"use strict";
+
+//var a = 20;
+function sum(x, b) {
+    a = x + b;
+    console.log(a);
+}
+
+console.log(a);
+sum(10,20);
+var a = 50;
+console.log(a);
